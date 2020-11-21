bfc7a822a2484b660236b07993d25b89c1c1c78d
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-20 21:17:47 -0500

    getters and setters for the laptop class

---
+++ src/main/java/com/springboot/Laptop.java
@@ -0,0 +1,22 @@
+package com.springboot;
+
+public class Laptop {
+    private int lid;
+    private String brand;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+}
--- src/main/java/com/springboot/SpringHomeworkApplication.java
+++ src/main/java/com/springboot/SpringHomeworkApplication.java
@@ -10,6 +10,8 @@ public class SpringHomeworkApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringHomeworkApplication.class, args);
 		//configures the container
+		//with scope=prototype, the object is actually made twice
+		//not just being called upon twice in a spring container
 		Alien a = context.getBean(Alien.class);
 		a.show();
 		Alien a1 = context.getBean(Alien.class);
